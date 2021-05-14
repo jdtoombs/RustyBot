f275804679f86a75d382b58dee00e805801c74f6
jdtoombs <jdtoombs@gmail.com>
2021-05-14 14:57:57 -0700

    Converting to TypeScript
    Co-authored-by: Dillon Johnson <nollidnosnhoj@gmail.com>

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import Discord from "discord.js";
+import * as dotenv from "dotenv";
+
+const client = new Discord.Client();
+
+dotenv.config();
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user!.tag}!`);
+})
+
+client.on("message", msg => {
+    if (msg.content === "!test") {
+        msg.reply("testing");
+    }
+})
+
+client.login(process.env.TOKEN);
